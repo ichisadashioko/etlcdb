4cf8cf9107ba00070184fdd4a43786c041f3c711
shioko <ichisadashioko@gmail.com>
2021-02-05 02:48:15 +0000

    bottle: learn about routing and serving static files

--- etl2_clean_image_project/web_server/main.py
+++ etl2_clean_image_project/web_server/main.py
@@ -1,9 +1,17 @@
-from bottle import route, run, template
+import os
+import bottle
+
+SCRIPT_PARENT, SCRIPT_FILENAME = os.path.split(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(SCRIPT_PARENT, 'static')
+
+@bottle.route('/hello/<name>')
+def hello(name='Stranger'):
+    return bottle.template('hello {{name}}, how are you?', name=name)
 
 
-@route('/hello/<name>')
-def index(name):
-    return template('hello {{name}}', name=name)
+@bottle.route('/<filepath:path>')
+def serve_static(filepath):
+    return bottle.static_file(filepath, root=STATIC_ROOT)
 
 
-run(host='localhost', port=8888)
+bottle.run(host='localhost', port=8888, debug=True)
